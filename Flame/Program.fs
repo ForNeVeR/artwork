7f9e2ce8632537ec5d56cff09633794c381e9c75
Friedrich von Never <friedrich@fornever.me>
2020-07-05 21:37:59 +0700

    Add initial implementation of the Flame program

---
+++ Flame/Program.fs
@@ -0,0 +1,56 @@
+﻿open System
+open System.Drawing.Imaging
+open System.IO
+open System.Drawing
+
+let private printUsage() =
+    printfn "Arguments: <resolution> <cycles> <output>"
+
+let private gradations = 30
+
+let private createModel resolution =
+    Array2D.init resolution resolution (fun _ y ->
+        if y = 0
+        then gradations - 1
+        else 0
+    )
+
+let private processStep model =
+    Array2D.mapi (fun x y current ->
+        if y = 0 then current
+        else
+            let below = Array2D.get model x (y - 1)
+            Math.Clamp(below - 1, 0, gradations - 1)
+    ) model
+
+let private palette = [|
+     let mutable color = Color.Black
+     let increment = Byte.MaxValue / (byte gradations)
+     for _ in 1..(gradations - 1) do
+         color <- Color.FromArgb(int <| color.R + increment, int color.G, int color.B)
+         color
+     Color.Red
+|]
+
+let private saveImage model (output: Stream) =
+    use bitmap = new Bitmap(Array2D.length1 model, Array2D.length2 model)
+    Array2D.iteri (fun x y g ->
+        bitmap.SetPixel(x, bitmap.Height - y - 1, palette.[g])
+    ) model
+    bitmap.Save(output, ImageFormat.Png)
+
+let private generateImage resolution cycles output =
+    let mutable model = createModel resolution
+    for i in 1..cycles do
+        model <- processStep model
+    saveImage model output
+
+[<EntryPoint>]
+let main: string[] -> int = function
+| [| resolution; cycles; output |] ->
+    let resolution = int resolution
+    let cycles = int cycles
+    use output = new FileStream(output, FileMode.Create)
+    generateImage resolution cycles output
+    0
+| _ -> printUsage(); 1
--- Readme.md
+++ Readme.md
@@ -14,14 +14,27 @@ $ powershell ./export.ps1
 It will generate all the images with `black`, `white` and `none` backgrounds in
 the `png` directory.
 
+## Flame
+
+There's a program to generate flame images written in style of DOOM. Execute
+with .NET 3.1+ SDK:
+
+```
+$ dotnet run -- 256 80 T:\Temp\flame.png
+$ #  <resolution> <steps> <outfile>
+```
+
 ## License [![logo-cc-by][]][cc-by-license]
 
-This work is licensed under a [Creative Commons Attribution 4.0 International
-License][cc-by-license], with the exception of the `memes` directory.
+The images in this repositories are licensed under a [Creative Commons
+Attribution 4.0 International License][cc-by-license], with the exception of the
+ `memes` directory.
 
 The `memes` directory may contain copyrighted material the use of which has not
 always been specifically authorized by the copyright owner. It is believed that
 this constitutes a 'fair use' of any such copyrighted material as parody.
+
+The code in this repository is licensed under the [MIT license][mit-license].
 
 [cc-by-license]: https://creativecommons.org/licenses/by/4.0/
 
